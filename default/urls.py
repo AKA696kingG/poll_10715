bc03594bd98548b4b2a06197c5914f8d848301c1
unknown <a25403098@gmail.com>
2019-10-31 12:09:42 +0800

    init

---
+++ default/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('poll/', views.PollList.as_view()),
+    path('poll/<int:pk>/', views.PollDetail.as_view()),
+    path('option/<int:pk>/', views.PollVote.as_view()),
+    path('poll/create/', views.PollCreate.as_view()),
+    path('poll/<int:pk>/update/', views.PollUpdate.as_view()),
+    path('poll/<int:pk>/delete/', views.PollDelete.as_view()),
+    path('option/create/<int:pid>/', views.OptionCreate.as_view()),
+    path('option/<int:pk>/update/', views.OptionUpdate.as_view()),
+]
